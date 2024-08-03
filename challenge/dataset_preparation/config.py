333fd14939e1dcd1db9a3974e138c6ab85e94e11
lpardey <lpardey@outlook.com>
2024-08-03 15:47:34 +0000

    update how dates_range are generated

--- challenge/dataset_preparation/config.py
+++ challenge/dataset_preparation/config.py
@@ -1,6 +1,5 @@
 import numpy
 import numpy.typing
-import pandas
 from pydantic import BaseModel, ConfigDict
 
 DateArray = numpy.typing.NDArray[numpy.datetime64]
@@ -12,7 +11,7 @@ class Config(BaseModel):
     records: int = 10_000_000
     batch_size: int = 1_000_000
     number_of_batches: int = records // batch_size
-    dates_range: DateArray = pandas.date_range(start="2023-01-01", end="2023-12-31").to_numpy()
+    dates_range: DateArray = numpy.arange("2023-01", "2024-01", dtype="datetime64[D]")
     product_categories: list[str] = ["Moda", "Tecnología", "Belleza", "Salud", "Juguetes"]
     sale_regions: list[str] = ["Caribe", "Andina", "Pacífico", "Orinoquía", "Amazonía", "Insular"]
     random_number_generator: numpy.random.Generator = numpy.random.default_rng(12345)
