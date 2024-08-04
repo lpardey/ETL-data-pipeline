36e32ea64c1f8f56ea648f58a66fd8499b872dbd
lpardey <lpardey@outlook.com>
2024-08-04 18:34:44 +0000

    separate parser into its own module add missing arguments to parser

--- challenge/upload_to_google_cloud/main.py
+++ challenge/upload_to_google_cloud/main.py
@@ -1,29 +1,28 @@
-import argparse
 import logging
+import random
 import sys
 from pathlib import Path
 
 from google.cloud.exceptions import exceptions
 from google.cloud.storage import Bucket, Client, transfer_manager  # type: ignore
 
+from .parser import Config, get_config
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("Carga a Google Cloud Storage")
 
 
-def main() -> None:
-    parser = argparse.ArgumentParser(description="Script para cargar archivos parquet a Google Cloud Storage")
-    parser.add_argument("-d", "--directory", type=str, help="Ruta de directorio con archivos a cargar", required=True)
-    args = parser.parse_args()
-    source_directory = args.directory
+def main(argv: list[str] | None = None) -> None:
+    config = get_config(argv)
 
     try:
         client = Client()
         logger.info("Obteniendo bucket para carga de archivos...")
-        bucket = get_or_create_bucket(client, "my_celes_bucket")
+        bucket = get_or_create_bucket(client, config.bucket_name)
         logger.info(f"Iniciando carga de archivos al gcs bucket {bucket.name}")
-        upload_files_to_gcs(bucket, source_directory)
+        upload_files_to_gcs(bucket, config.directory)
     except FileNotFoundError:
-        logger.error(f"El directorio '{source_directory}' no existe")
+        logger.error(f"El directorio '{config.directory}' no existe")
         sys.exit(1)
     except Exception as e:
         logger.error(f"Ocurrió un error inesperado: {e}")
@@ -40,8 +39,8 @@ def get_or_create_bucket(client: Client, bucket_name: str) -> Bucket:
     return bucket
 
 
-def upload_files_to_gcs(bucket: Bucket, source_directory: str, workers: int = 8) -> None:
-    source_dir_path_obj = Path(source_directory)
+def upload_files_to_gcs(bucket: Bucket, config: Config) -> None:
+    source_dir_path_obj = Path(config.directory)
     paths = list(source_dir_path_obj.rglob("*"))
 
     if not paths:
@@ -49,17 +48,21 @@ def upload_files_to_gcs(bucket: Bucket, source_directory: str, workers: int = 8)
 
     if len(paths) == 1:
         filename = str(paths[0])
-        blob = bucket.blob("my_celes_blob")
+        blob_name = f"my_blob_{random.randrange(1000, 2000)}" if config.blob_name is None else config.blob_name
+        blob = bucket.blob(blob_name)
         blob.upload_from_filename(filename, if_generation_match=0)
         logger.info(f"Archivo '{filename}' cargado al bucket '{bucket.name}'.")
 
     else:
         file_paths = [path for path in paths if path.is_file() and path.suffix == ".parquet"]
-        filenames = [str(path.relative_to(source_directory)) for path in file_paths]
+        filenames = [str(path.relative_to(config.directory)) for path in file_paths]
         logger.info(f"Se encontraron {len(filenames)} archivos a cargar")
         # Uploads multiple objects concurrently
         results = transfer_manager.upload_many_from_filenames(
-            bucket=bucket, filenames=filenames, source_directory=source_directory, max_workers=workers
+            bucket=bucket,
+            filenames=filenames,
+            source_directory=config.directory,
+            max_workers=config.workers,
         )
         for name, result in zip(filenames, results):
             if isinstance(result, Exception):
