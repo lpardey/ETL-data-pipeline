70b08a93b8ce5fe11ba4f68686fdb43853249ddd
lpardey <lpardey@outlook.com>
2024-08-28 00:39:47 +0000

    implement polars to transform and load to refactor

--- challenge/transform_load_to_parquet/main.py
+++ challenge/transform_load_to_parquet/main.py
@@ -3,71 +3,75 @@ import sys
 from pathlib import Path
 from typing import Literal
 
-import pyarrow
+import polars as pl
 import pyarrow.csv
 import pyarrow.parquet
 
 from .parser import get_config
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("Carga y transformación a Parquet")
+logger = logging.getLogger("Transform and load to Parquet")
 
 
 class ErrorHandler:
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: Path) -> None:
         self.path = path
         self.records: list[str] = []
 
-    def register_error(self, record) -> Literal["skip"]:
+    def register_error(self, record: str) -> Literal["skip"]:
         self.records.append(record)
         return "skip"
 
     def save(self) -> None:
         if self.records:
-            logger.warning(f"Encontrados {len(self.records)} inválidos. Enviando registros a archivo '{self.path}'")
-        with open(self.path, "a") as f:
-            for row in self.records:
-                f.write(row + "\n")
+            logger.warning(f"{len(self.records)} invalid records. Saving records to '{self.path}'")
+            with open(self.path, "a") as f:
+                f.writelines(self.records)
 
 
 def main(argv: list[str] | None = None) -> None:
     config = get_config(argv)
+    logging.basicConfig(level=config.log_level)
 
     try:
-        if not Path(config.input).exists():
-            raise FileNotFoundError(f"El archivo de entrada {config.input} no existe.")
-        if not config.force and Path(config.output).exists():
-            raise FileExistsError(f"El destino {config.output} ya existe.")
-        Path(config.output).mkdir(exist_ok=True)
+        if not config.input.exists():
+            raise FileNotFoundError(f"Input file '{config.input.name}' doesn't exist.")
 
-        logger.info("Leyendo archivo a transformar")
-        quarantine = ErrorHandler(config.output + "/quarantine.csv")
-        parse_options = pyarrow.csv.ParseOptions(invalid_row_handler=quarantine.register_error)
-        table = pyarrow.csv.read_csv(config.input, parse_options=parse_options)
-        quarantine.save()
+        if not config.force and config.output.exists():
+            raise FileExistsError("The file already exists. Add '-f' flag to overwrite it, or choose another name.")
 
-        logger.info("Transformando a formato Parquet")
-        pyarrow.parquet.write_to_dataset(
-            table=table,
-            root_path=config.output,
-            partition_cols=config.partition_cols,
-        )
+        config.output.parent.mkdir(exist_ok=True)
 
-        logger.info("Lectura y transformación de datos completada")
+        if not config.error_handling:
+            logger.info("Reading file (ignore errors)")
+            df = pl.read_csv(config.input, ignore_errors=True)
 
-    except FileNotFoundError:
-        logger.exception("El archivo de entrada no existe")
-    except FileExistsError:
-        logger.exception("El destino a escribir ya existe.")
+            logger.info("Generating Parquet file")
+            file = config.output if config.partition_cols is None else config.output.parent
+            # 'snappy' guarantees more backwards compatibility when dealing with older parquet readers
+            # https://docs.pola.rs/api/python/stable/reference/api/polars.DataFrame.write_parquet.html
+            df.write_parquet(file, compression="snappy", partition_by=config.partition_cols)
+        else:
+            logger.info("Reading file")
+            quarantine = ErrorHandler(config.output.parent / "quarantine.csv")
+            parse_options = pyarrow.csv.ParseOptions(invalid_row_handler=quarantine.register_error)
+            table = pyarrow.csv.read_csv(config.input, parse_options=parse_options)
+            quarantine.save()
+
+            logger.info("Generating Parquet file")
+            pyarrow.parquet.write_to_dataset(
+                table=table, root_path=config.output.parent, partition_cols=config.partition_cols
+            )
+
+        logger.info(f"Successfully transform and load '{config.input.name}' to Parquet")
+
+    except FileNotFoundError as e:
+        logger.exception(e)
         sys.exit(1)
-    except pyarrow.ArrowInvalid as e:
-        logger.exception(f"Error al leer el archivo: {e}")
-        sys.exit(1)
-    except pyarrow.ArrowException as e:
-        logger.exception(f"Error al escribir el archivo: {e}")
+    except FileExistsError as e:
+        logger.exception(e)
         sys.exit(1)
     except Exception as e:
-        logger.exception(f"Ocurrió un error inesperado: {e}")
+        logger.exception(f"An error occurred: {e}")
         sys.exit(1)
 
 
--- challenge/transform_load_to_parquet/parser.py
+++ challenge/transform_load_to_parquet/parser.py
@@ -1,29 +1,49 @@
 import argparse
+import logging
+from pathlib import Path
 
 from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    input: str
-    output: str
+    input: Path
+    output: Path
     partition_cols: list[str] | None
     force: bool
+    error_handling: bool
+    log_level: int
 
 
 def get_parser() -> argparse.ArgumentParser:
-    prog = "Carga y transformación a Parquet"
-    description = "Este programa recibe archivos de texto plano y los transforma a formato Parquet"
-    partition_cols_help = "Nombres de columnas mediante las cuales se hará la partición del conjunto de datos. El comportamiento por defecto no realiza particiones"
+    prog = "Transform and load to Parquet"
+    description = "Convert a plain text file into a Parquet file with optional partitioning and error handling."
+    output_help = "Path to write the output Parquet file."
+    partition_cols_help = "Column names by which the dataset will be partitioned. No partitions are made by default."
+    error_handling_help = (
+        "Errors encountered while reading the file will be captured and stored in a quarantine file. "
+        "This method is slower and results in a larger output file compared to the default behavior."
+    )
+    log_level_help = "Set the root logger level to the specified level."
+    log_level_choices = ("NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL")
 
     parser = argparse.ArgumentParser(prog=prog, description=description)
-    parser.add_argument("input", type=str, help="Archivo a transformar")
-    parser.add_argument("-o", "--output", type=str, help="Destino a escribir", default="./parquet")
+    parser.add_argument("input", type=Path, help="Plain text file to transform")
+    parser.add_argument("output", type=Path, help=output_help)
     parser.add_argument("-p", "--partition-cols", help=partition_cols_help, nargs="*")
-    parser.add_argument("-f", "--force", help="Sobreescribir en destino", action="store_true", default=False)
+    parser.add_argument("-f", "--force", help="Overwrite output file", action="store_true")
+    parser.add_argument("-e", "--error-handling", help=error_handling_help, action="store_true")
+    parser.add_argument("-l", "--log-level", help=log_level_help, choices=log_level_choices, default="INFO")
     return parser
 
 
 def get_config(argv: list[str] | None = None) -> Config:
     parser = get_parser()
     args = parser.parse_args(argv)
-    return Config(input=args.input, output=args.output, partition_cols=args.partition_cols, force=args.force)
+    return Config(
+        input=args.input,
+        output=args.output,
+        partition_cols=args.partition_cols,
+        force=args.force,
+        error_handling=args.error_handling,
+        log_level=getattr(logging, args.log_level),
+    )
